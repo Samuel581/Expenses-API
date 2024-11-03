a7ba0a25e109a759540e02544f4be302fa5996d2
Samuel Adonay Ortiz Carballo <00023020@uca.edu.sv>
2024-11-03 14:56:31 -0600

    ✨ Add expense routes

---
+++ src/routes/expense.router.ts
@@ -0,0 +1,12 @@
+import * as expenseController from '../controllers/expense..controller'
+import { Router } from 'express'
+import verifyToken from '../middlewares/auth.middleware';
+
+const expenseRouter = Router();
+
+expenseRouter.post('/', verifyToken, expenseController.createExpense);
+expenseRouter.patch('/:id', verifyToken, expenseController.updateExpense);
+expenseRouter.delete('/:id', verifyToken, expenseController.deleteExpense);
+expenseRouter.get('/', verifyToken, expenseController.getAllExpenses);
+
+export default expenseRouter;
