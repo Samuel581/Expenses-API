9f1fe12e1b4e31dbecfd71dc0591f0e35aaa3032
Samuel Adonay Ortiz Carballo <00023020@uca.edu.sv>
2024-11-06 23:54:51 -0600

    ✨ Add and implements cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response } from 'express';
 import dotenv from 'dotenv';
-import mainRouter from './routes/main.router';
+import mainRouter from './routes/main.router'; 
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 //Build in express needed middlewares
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000']
+}))
 //JSON
 app.use(express.json());
 //Format endpoints
