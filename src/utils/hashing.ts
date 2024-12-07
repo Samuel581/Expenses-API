6f5cdfec3d329272e41c9cc51a4eeaa4621b8d31
Samuel Adonay Ortiz Carballo <00023020@uca.edu.sv>
2024-12-07 14:36:55 -0600

    :memo: Update documentation on hashing utils

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -1,9 +1,11 @@
 import bcrypt from 'bcryptjs';
 
+//Hashes passwords before inserting them into the database
 export const hashPassword = async (password: string) => {
   return await bcrypt.hash(password, 12);
 };
 
+//Verifies and decrypts the password that the user provided, if it matches it continues the login process
 export const verifyPassword = async (password: string, hashedPassword: string) => {
   return await bcrypt.compare(password, hashedPassword);
 };
